4d3e75329f46b15c9ae15387ff562960e20128f7
matt brandt <mattbrandt87@gmail.com>
2014-05-12 15:39:01 -0500

    latest

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,14 +3,25 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
+
+    if @address.nil
+        # asdf
+    else
+        # as
+    end
+
     @address = "the corner of Foster and Sheridan"
+
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    url = "http://maps.googleapis.com/maps/api/geocode/json?#{@url_safe_address}&sensor=false"
+
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    # results = parsed_data["results"]
+
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
   end
 end
